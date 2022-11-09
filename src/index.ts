404c6882bd6577f98f119ceb5f906a56184a509a
fsvobxz <116634964+fsvobxz@users.noreply.github.com>
2022-11-09 12:47:55 +0100

    getToken and getNFT functions

--- src/index.ts
+++ src/index.ts
@@ -12,5 +12,26 @@ const NFTLIST: { [key in AssetSupportedChainId]: Asset[]} = {
   [ChainId.Testnet]: TESTNET_NFTS,
 }
 
+/**
+ * Utility function to find and return a token based on its address and chainId.
+ * 
+ * @param address Address hash
+ * @param chainId Chain ID out of AssetSupportedChainId enum
+ * @returns Asset type, undefined if not found.
+ */
+const getToken = (address:string, chainId = AssetSupportedChainId.Mainnet) => {
+  return TOKENLIST[chainId].find((entry) => entry.address.toLowerCase() === address.toLowerCase())
+};
 
-export { TOKENLIST, NFTLIST, Asset, AssetNFT, AssetType }
+/**
+ * Utility function to find and return an NFT based on its address and chainId.
+ * 
+ * @param address Address hash
+ * @param chainId Chain ID out of AssetSupportedChainId enum
+ * @returns AssetNFT type, undefined if not found.
+ */
+const getNFT = (address:string, chainId = AssetSupportedChainId.Mainnet) => {
+  return NFTLIST[chainId].find((entry) => entry.address.toLowerCase() === address.toLowerCase())
+};
+
+export { TOKENLIST, NFTLIST, getToken, getNFT, Asset, AssetNFT, AssetType }
