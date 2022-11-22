d3a81abb6b14b90dcfeb1c64b8a94763ca61942b
fsvobxz <116634964+fsvobxz@users.noreply.github.com>
2022-11-22 10:09:44 +0100

    2 more tests - image format and existence

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const BASE_DOMAIN = 'https://asset.callisto.network/';
+export const BASE_FULL_URL = BASE_DOMAIN + 'images/coins/';
+export const BASE_EXTENSION = '.png';
--- test/jsons.test.ts
+++ test/jsons.test.ts
@@ -1,4 +1,6 @@
 import Web3 from 'web3';
+import fs from 'fs';
+import { BASE_DOMAIN, BASE_FULL_URL, BASE_EXTENSION } from '../src/constants';
 import { MAINNET_TOKENS, MAINNET_NFTS } from '../src/chains/mainnet';
 import { TESTNET_TOKENS, TESTNET_NFTS } from '../src/chains/testnet';
 
@@ -6,26 +8,30 @@ const data = [
   {
     assets: MAINNET_TOKENS,
     isNFT: false,
+    chainId: 820,
     toString: () => 'Mainnet Tokens',
   },
   {
     assets: MAINNET_NFTS,
     isNFT: true,
+    chainId: 820,
     toString: () => 'Mainnet NFTs',
   },
   {
     assets: TESTNET_TOKENS,
     isNFT: false,
+    chainId: 20729,
     toString: () => 'Testnet Tokens',
   },
   {
     assets: TESTNET_NFTS,
     isNFT: true,
+    chainId: 20729,
     toString: () => 'Testnet NFTs',
   },
 ];
 
-describe.each(data)('JSON validation - %s', ({ assets, isNFT }) => {
+describe.each(data)('JSON validation - %s', ({ assets, isNFT, chainId }) => {
   it('does not contain duplicate names or addresses', () => {
     for (let i = 0; i < assets.length; i++) {
       for (let j = 0; j < assets.length; j++) {
@@ -33,9 +39,9 @@ describe.each(data)('JSON validation - %s', ({ assets, isNFT }) => {
           expect(assets[i].name.toLowerCase()).not.toBe(
             assets[j].name.toLowerCase()
           );
-          //expect(assets[i].symbol.toLowerCase()).not.toBe(
-          //  assets[j].symbol.toLowerCase()
-          //); // COMMENTED OUT - CAN BE IDENTICAL FOR TWO ERC STANDARDS
+
+          // Symbol can be identical for two ERC standards
+
           expect(assets[i].address.toLowerCase()).not.toBe(
             assets[j].address.toLowerCase()
           );
@@ -56,7 +62,28 @@ describe.each(data)('JSON validation - %s', ({ assets, isNFT }) => {
 
   it('does not contain invalid address', () => {
     for (let item of assets) {
-      expect(Web3.utils.isAddress(item.address)).toBe(true);
+      expect(Web3.utils.isAddress(item.address) && item.address).toBe(
+        item.address
+      );
+    }
+  });
+
+  it('image address has correct format', () => {
+    if (!isNFT) {
+      for (let item of assets) {
+        expect(item.image).toBe(
+          BASE_FULL_URL + chainId + '/' + item.address + BASE_EXTENSION
+        );
+      }
+    }
+  });
+
+  it('image source exists', () => {
+    if (!isNFT) {
+      for (let item of assets) {
+        const localPath = item.image.replace(BASE_DOMAIN, './public/');
+        expect(fs.existsSync(localPath) && item.address).toBe(item.address);
+      }
     }
   });
 });
