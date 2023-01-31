32fbc34f79681fc5fee6bbb13b4bd0d3179b3972
Klein Petr <petr@kleinpetr.com>
2023-01-31 13:32:32 +0100

    update utils to accept undefined riskLevel

--- src/utils.ts
+++ src/utils.ts
@@ -1,17 +1,18 @@
 import { AuditRiskLevel } from "./types"
 
-export const getRiskLevelDescription = (riskLevel: AuditRiskLevel) => {
+export const getRiskLevelDescription = (riskLevel?: AuditRiskLevel) => {
+    if(!riskLevel) return 'Audited'
     if(riskLevel === AuditRiskLevel.Safe) return 'Audit: SAFE'
     return `Audit: ${riskLevel} risk` 
 }
 
-export const getRiskLevelColor = (riskLevel: AuditRiskLevel) => {
+export const getRiskLevelColor = (riskLevel?: AuditRiskLevel) => {
     switch (riskLevel) {
         case AuditRiskLevel.Safe: return '#0b942b'
         case AuditRiskLevel.Low: return '#43c861'
         case AuditRiskLevel.Medium: return '#ef801a'
         case AuditRiskLevel.High: return '#c22821'
         case AuditRiskLevel.Critical: return '#680702'
-        default: return '#E0E0E0'
+        default: return '#0b942b'
     }
 }
